ee4f37e72afe6fd4658c32262041272e1fcd7d18
egiorgana <eagiorganamorales@gmail.com>
2020-03-05 12:02:05 -0600

    dia 5

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.MONGO_URI
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const connection = mongoose.connection
+connection.once('open', () => console.log('DB Connected 👨🏼‍🚀'))
+
+const articlesRouter = require('./routes/articles')
+app.use('/articles', articlesRouter)
+
+app.listen(port, () => console.log('Server Connected 👽'))
